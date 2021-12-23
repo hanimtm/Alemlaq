ed5cd0fbce048207ca53e8ac5ebe73ee36fc9131
hiren-acespritech <hirenatroliya.acespritech@gmail.com>
2021-12-23 10:23:51 +0530

    [REM] Remove module.

--- aspl_sales_customisation/models/res_partner.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import fields, models, api, _
-
-
-class SaleType(models.Model):
-    _inherit = 'res.partner'
-
-    sales_type_id = fields.Many2one('sale.type', required=True, string='Sales Type')
