ae29ec2fbabc7a6c51b8f6a2a5fe2dd754346224
harsh-acespritech <harsh.acespritech@gmail.com>
2022-01-04 18:05:11 +0530

    [MOD] Remove the old one module 'aspl_company_branch_ee'

--- aspl_company_branch_ee/models/product.py
+++
@@ -1,8 +0,0 @@
-from odoo import models, fields, api, _
-from odoo.exceptions import UserError
-
-
-class ProductTemplate(models.Model):
-    _inherit = 'product.template'
-
-    branch_id = fields.Many2many('company.branch', string="Branch", required=True)
