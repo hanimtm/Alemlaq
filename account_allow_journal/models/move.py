fba39e7dcc58d1c29cbff94a00b16f525281cf69
Aneesh AV <aneesh@amcde.com>
2022-03-10 16:01:34 +0300

    [ADD] Journal Modules

--- account_allow_journal/models/move.py
+++ account_allow_journal/models/move.py
@@ -5,11 +5,10 @@ from odoo import models, fields, api
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
-    suitable_journal_ids = fields.Many2many('account.journal', compute='_compute_suitable_journal_new_ids')
-
     @api.depends('company_id', 'invoice_filter_type_domain')
-    def _compute_suitable_journal_new_ids(self):
+    def _compute_suitable_journal_ids(self):
         for m in self:
+            journal_type = m.invoice_filter_type_domain or 'general'
             company_id = m.company_id.id or self.env.company.id
             domain = [('company_id', '=', company_id)]
             m.suitable_journal_ids = self.env['account.journal'].search(domain)
