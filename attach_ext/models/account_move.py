5c037495a81a1cf90b149db9983cd38492e9287c
ibrahimkaddour <ibrahimkaddour@gmail.com>
2022-05-29 14:48:32 +0300

    attach file on payment

--- attach_ext/models/account_move.py
+++ attach_ext/models/account_move.py
@@ -5,15 +5,61 @@ import logging
 _logger = logging.getLogger(__name__)
 
 
-# class PaymentRegister(models.Model):
-#     _inherit = 'account.payment.register'
-#
-#     transfer_no = fields.Char(string='Transfer Number')
-#     transfer_permit = fields.Binary(string='Transfer Permit')
-#     transfer_permit_filename = fields.Char(string='Transfer Permit')
-#     # bank_name = fields.Many2one(comodel_name='account.journal', string='Bank',
-#     #                             domain="[('type', '=', 'bank')]")
+class AccountPayment(models.Model):
+    _inherit = 'account.payment'
 
+    transfer_no = fields.Char(string='Transfer Number')
+    transfer_permit = fields.Binary(string='Transfer Permit')
+    transfer_permit_filename = fields.Char(string='Transfer Permit')
+
+
+class PaymentRegister(models.TransientModel):
+    _inherit = 'account.payment.register'
+
+    transfer_no = fields.Char(string='Transfer Number')
+    transfer_permit = fields.Binary(string='Transfer Permit')
+    transfer_permit_filename = fields.Char(string='Transfer Permit')
+    # bank_name = fields.Many2one(comodel_name='account.journal', string='Bank',
+    #                             domain="[('type', '=', 'bank')]")
+
+    # def _create_payment_vals_from_batch(self, batch_result):
+    #     batch_values = self._get_wizard_values_from_batch(batch_result)
+    #     return {
+    #         'date': self.payment_date,
+    #         'amount': batch_values['source_amount_currency'],
+    #         'payment_type': batch_values['payment_type'],
+    #         'partner_type': batch_values['partner_type'],
+    #         'ref': self._get_batch_communication(batch_result),
+    #         'journal_id': self.journal_id.id,
+    #         'currency_id': batch_values['source_currency_id'],
+    #         'partner_id': batch_values['partner_id'],
+    #         'partner_bank_id': batch_result['payment_values']['partner_bank_id'],
+    #         'payment_method_line_id': self.payment_method_line_id.id,
+    #         'destination_account_id': batch_result['lines'][0].account_id.id
+    #     }
+
+    def _create_payment_vals_from_batch(self, batch_result):
+        _logger.critical('3333333333333333333333333333333')
+        res = super(PaymentRegister, self)._create_payment_vals_from_batch(batch_result)
+        # if self.env.context.get('active_model') == 'account.move' and self.env.context.get('active_ids'):
+        #     move_ids = self.env['account.move'].search([('id', 'in', self.env.context.get('active_ids'))])
+        #     check_branch_diff = set([x.branch_id.id for x in move_ids])
+        #     if len(check_branch_diff) > 1:
+        #         raise UserError(_("Cannot create payment for different branches."))
+        #     elif len(check_branch_diff) == 1:
+        _logger.critical('**********************************')
+        _logger.critical(batch_result)
+        res['transfer_no'] = self.transfer_no
+        res['transfer_permit'] = self.transfer_permit
+        res['transfer_permit_filename'] = self.transfer_permit_filename
+        return res
+
+    def _create_payment_vals_from_wizard(self):
+        res = super(PaymentRegister, self)._create_payment_vals_from_wizard()
+        res['transfer_no'] = self.transfer_no
+        res['transfer_permit'] = self.transfer_permit
+        res['transfer_permit_filename'] = self.transfer_permit_filename
+        return res
 
 class AccountMove(models.Model):
     _inherit = 'account.move'
